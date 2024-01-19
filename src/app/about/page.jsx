709a9e9bfcf3e1d2fe4a26de796f04b121304373
Nory Lazo <nlazo@me.com>
2024-01-18 19:09:57 -0700

    Creating pages.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import Image from 'next/image';
+import styles from './about.module.css';
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean cursus ligula et dolor
+          vestibulum ornare. Vestibulum tempus dolor augue, id dapibus tellus sodales nec. Duis at
+          dolor facilisis, gravida turpis at, mattis mauris.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Years of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Years of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Years of experience</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.imgContainer}>
+        <Image src='/about.png' alt='About image' fill />
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import styles from './footer.module.css';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>lamadev</div>
+      <div className={styles.text}>Lama creative thoughts agency &copy; All rights reserved.</div>
+    </div>
+  );
 };
 
 export default Footer;
